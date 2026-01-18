cb2629b3d48ab62fb1b62c6501a359a0e85b53f1
unknown <dakshsaxxenaa@gmail.com>
2026-01-18 23:55:35 +0530

    Final Changes done

--- frontend/src/pages/Wishlist.jsx
+++ frontend/src/pages/Wishlist.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useEffect, useState } from "react";
 import { getWishlist, removeFromWishlist } from "../services/wishlist.service";
 import { useNavigate } from "react-router-dom";
